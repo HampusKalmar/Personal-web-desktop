80e515fc99fa3d2b91285bd30c175fb94e2bdc09
Hampus Tuisku <ht222mi@student.lnu.se>
2023-06-12 12:31:53 +0200

    Add a method that represents all winning combinations

--- src/js/components/tic-tac-toe/tic-tac-toe.js
+++ src/js/components/tic-tac-toe/tic-tac-toe.js
@@ -41,8 +41,51 @@ customElements.define('tic-tac-toe',
       this.#currentTurn = 'x'
       for (let i = 0; i < 9; i++) {
         const cell = document.createElement('div')
-        // cell.addEventListener('click', () =>)
+        cell.addEventListener('click', () => this.handleCellClick(cell))
         this.#board.appendChild(cell)
       }
+    }
+
+    /**
+     * Handles a click event on a cell in the game.
+     *
+     * @param {HTMLElement} cell - The clicked cell element.
+     */
+    handleCellClick (cell) {
+      if (cell.textContent !== '') {
+        cell.textContent = this.#currentTurn
+        cell.classList.add(this.#currentTurn)
+      }
+      if (this.checkWin(this.#currentTurn)) {
+        this.#status.textContent = 'You Win'
+        setTimeout(() => this.startGame(), 2000)
+      } else {
+        this.#currentTurn = 'o'
+      }
+    }
+
+    /**
+     * Checks if the player has won the game.
+     *
+     * @param {string} player - The player to check.
+     * @returns {boolean} - True if the player has won, false if the computer won.
+     */
+    checkWin (player) {
+      const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+      ]
+      for (const line of lines) {
+        if (line.every(index => this.#board.children[index].textContent === player)) {
+          return true
+        }
+      }
+      return false
     }
   })
--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,3 @@
 import './components/sub-window/sub-window.js'
 import './components/message-application/message-application.js'
+import './components/tic-tac-toe/tic-tac-toe.js'
